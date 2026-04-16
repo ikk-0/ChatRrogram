9f4a824f05f0d8f463e86c58ca2a9deb1282fbf5
ik <2921283095@qq.com>
2026-04-16 15:44:10 +0800

    Delete build/Desktop_Qt_6_10_3_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_10_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TcpClient_FILE D:/WorkApps/QT/Workfiles/Project/TcpClient/build/Desktop_Qt_6_10_3_MinGW_64_bit-Debug/TcpClient.exe)
-set(__QT_DEPLOY_TARGET_TcpClient_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TcpClient_RUNTIME_DLLS D:/WorkApps/QT/6.10.3/mingw_64/bin/Qt6Widgets.dll;D:/WorkApps/QT/6.10.3/mingw_64/bin/Qt6Gui.dll;D:/WorkApps/QT/6.10.3/mingw_64/bin/Qt6Core.dll)
